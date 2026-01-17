cf4bbfcfeac2c2b87f9aa4b59dcaf3024acb9510
Aldair Rutte <xxpiltisxx@gmail.com>
2026-01-17 17:33:03 -0500

    Finished the individual branch

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import ProductData from './ProductData.mjs'
+import ProductList from './ProductList.mjs'
+
+const product_list = document.querySelector(".product-list")
+
+const dataSource = new ProductData("tents");
+
+const productList = new ProductList("tents", dataSource, product_list)
+productList.init()
+
