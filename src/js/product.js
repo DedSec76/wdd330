47d7dc15cdcc3061c87b5ba44bae8661a461ffef
Aldair Rutte <xxpiltisxx@gmail.com>
2026-01-19 16:49:18 -0500

    Some trello tasks were completed week2.v2

--- src/js/product.js
+++ src/js/product.js
@@ -2,7 +2,7 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam('product')
+const productId = getParam("product")
 const dataSource = new ProductData("tents");
 
 const tents = new ProductDetails(productId, dataSource)
